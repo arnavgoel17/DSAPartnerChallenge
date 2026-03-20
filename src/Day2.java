69b787a6a69fc5b47de5a9ce080c745379f99263
Arnav Goel <arnavgoel2006@gmail.com>
2026-03-20 14:36:04 +0530

    Rename Day1.java to Day2.java

--- src/Day2.java
+++ src/Day2.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Day1 {
+public class Day2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
